73f54932a9a95cfb0174e76ebbbc141f2e528daf
Cosmin-Petru Nechifor <cosmin.nechifor96@gmail.com>
2018-10-03 22:43:43 +0300

    database creation is automated, needs changes

--- src/init_db.py
+++ src/init_db.py
@@ -1,9 +1,20 @@
 import argparse
 import pathlib
+import aiopg.sa
+import trafaret as T
 
-from sqlalchemy import create_engine, MetaData
+from trafaret_config import commandline
+from sqlalchemy import create_engine
 
-# from src.tools.db import user
+from sqlalchemy import (
+    MetaData,
+    Table,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Date,
+)
 
 DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
 
@@ -11,24 +22,148 @@ ADMIN_DB_URL = DSN.format(
     user='postgres', password='postgres', database='postgres',
     host='localhost', port=5432
 )
-print(ADMIN_DB_URL)
 
-# admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
+TRAFARET = T.Dict({
+    T.Key('postgres'):
+        T.Dict({
+            'database': T.String(),
+            'user': T.String(),
+            'password': T.String(),
+            'host': T.String(),
+            'port': T.Int(),
+            'minsize': T.Int(),
+            'maxsize': T.Int(),
+        }),
+    T.Key('host'): T.IP,
+    T.Key('port'): T.Int(),
+})
+
+__all__ = ['title_table', 'user_table']
+
+meta = MetaData()
+
+title_table = Table(
+    'title', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(70), nullable=False),
+    Column('description', String(300), nullable=False)
+)
+
+user_table = Table(
+    'user', meta,
+
+    Column('id', Integer, primary_key=True),
+    Column('first_name', String(50), nullable=False),
+    Column('second_name', String(50), nullable=False),
+    Column('hiring_date', Date, nullable=False),
+
+    Column('title_id',
+           Integer,
+           ForeignKey('title.id', ondelete='CASCADE'))
+)
 
 
-def get_config_path():
-    path = pathlib.Path(__file__).parent.parent
-    print(path)
+async def init_pg(app):
+    conf = app['config']['postgres']
+    engine = await aiopg.sa.create_engine(
+        database=conf['database'],
+        user=conf['user'],
+        password=conf['password'],
+        host=conf['host'],
+        port=conf['port'],
+        minsize=conf['minsize'],
+        maxsize=conf['maxsize'],
+    )
+    app['db'] = engine
 
-# USER_CONFIG_PATH = BASE_DIR / 'config' / 'polls.yaml'
-# USER_CONFIG = get_config(['-c', USER_CONFIG_PATH.as_posix()])
-# USER_DB_URL = DSN.format(**USER_CONFIG['postgres'])
-# user_engine = create_engine(USER_DB_URL)
-#
-# TEST_CONFIG_PATH = BASE_DIR / 'config' / 'polls_test.yaml'
-# TEST_CONFIG = get_config(['-c', TEST_CONFIG_PATH.as_posix()])
-# TEST_DB_URL = DSN.format(**TEST_CONFIG['postgres'])
-# test_engine = create_engine(TEST_DB_URL)
+
+async def close_pg(app):
+    app['db'].close()
+    await app['db'].wait_closed()
+
+
+async def get_users(conn, user_id):
+    result = await conn.execute(
+        user_table.select().where(user_table.id == user_id),
+    )
+    user_record = await result.first()
+    if not user_record:
+        raise ValueError(f'User with id: {user_id} does not exist')
+    return user_record
+
+
+def get_config(configuration_path, argv=None):
+    ap = argparse.ArgumentParser()
+    commandline.standard_argparse_options(
+        ap,
+        default_config=configuration_path,
+    )
+
+    options, unknown = ap.parse_known_args(argv)
+    config = commandline.config_from_options(options, TRAFARET)
+
+    return config
+
+
+def create_configuration():
+    base_path = pathlib.Path(__file__).parent.parent
+    configuration_path = base_path / 'config' / 'configuration.yaml'
+    user_config = get_config(
+        configuration_path,
+        ['-c', configuration_path.as_posix()],
+    )
+
+    return user_config
+
+
+def setup_db(config):
+    db_name = config['database']
+    db_user = config['user']
+    db_pass = config['password']
+
+    admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
+    conn = admin_engine.connect()
+    conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
+    conn.execute("DROP ROLE IF EXISTS %s" % db_user)
+    conn.execute("CREATE USER %s WITH PASSWORD '%s'" % (db_user, db_pass))
+    conn.execute("CREATE DATABASE %s ENCODING 'UTF8'" % db_name)
+    conn.execute("GRANT ALL PRIVILEGES ON DATABASE %s TO %s" %
+                 (db_name, db_user))
+    conn.close()
+
+
+def create_tables(engine):
+    meta = MetaData()
+    meta.create_all(bind=engine, tables=[user_table, title_table])
+
+
+def drop_tables(engine):
+    meta = MetaData()
+    meta.drop_all(bind=engine, tables=[user_table, title_table])
+
+
+def create_test_data(engine):
+    conn = engine.connect()
+    conn.execute(title_table.insert(), [
+        {'id': 2,
+         'name': 'asdddd',
+         'description': 'asddddqwe',
+         },
+    ])
+    conn.execute(user_table.insert(), [
+        {'id': 3,
+         'first_name': 'first_name',
+         'second_name': 'second_name',
+         'hiring_date': '2018-10-27 21:20:32.619+01',
+         'title_id': 2},
+    ])
+    conn.close()
+
 
 if __name__ == '__main__':
-    get_config_path()
+    config = create_configuration()
+    setup_db(config['postgres'])
+    database_url = DSN.format(**config['postgres'])
+    user_engine = create_engine(database_url)
+    create_tables(engine=user_engine)
+    create_test_data(engine=user_engine)
